b34c8bce0f65c39e85437f16a2da75363fc91a49
Thiago Andrade <thiagoh@gmail.com>
2017-04-15 19:05:54 -0400

    cars and bg update

--- js/SpaceHero.js
+++ js/SpaceHero.js
@@ -1,4 +1,4 @@
-(function (window) {
+(function(window) {
 
     window.game = window.game || {}
 
@@ -14,7 +14,7 @@
     p.currentScene;
 
 
-    p.initialize = function () {
+    p.initialize = function() {
         // set globals
         canvas = document.getElementById('canvas');
         stage = new createjs.Stage(canvas);
@@ -26,7 +26,7 @@
         game.assets = new game.AssetManager();
         this.preloadAssets()
     }
-    p.preloadAssets = function () {
+    p.preloadAssets = function() {
         this.preloader = new ui.Preloader('#d2354c', '#FFF');
         this.preloader.x = (canvas.width / 2) - (this.preloader.width / 2);
         this.preloader.y = (canvas.height / 2) - (this.preloader.height / 2);
@@ -35,26 +35,26 @@
         game.assets.on(game.assets.ASSETS_COMPLETE, this.assetsReady, this);
         game.assets.preloadAssets();
     }
-    p.onAssetsProgress = function () {
+    p.onAssetsProgress = function() {
         this.preloader.update(game.assets.loadProgress);
         stage.update();
     }
-    p.assetsReady = function () {
+    p.assetsReady = function() {
         stage.removeChild(this.preloader);
         stage.update();
         this.createSpriteSheet();
         this.gameReady();
     }
-    p.createSpriteSheet = function () {
+    p.createSpriteSheet = function() {
         var assets = game.assets;
         spritesheet = new createjs.SpriteSheet(assets.getAsset(assets.GAME_SPRITES_DATA));
     }
-    p.gameReady = function () {
+    p.gameReady = function() {
         createjs.Ticker.setFPS(60);
         createjs.Ticker.on("tick", this.onTick, this);
         this.changeState(game.GameStates.MAIN_MENU);
     }
-    p.changeState = function (state) {
+    p.changeState = function(state) {
         switch (state) {
             case game.GameStates.MAIN_MENU:
                 this.currentGameStateFunction = this.gameStateMainMenu;
@@ -70,49 +70,49 @@
                 break;
         }
     }
-    p.onStateEvent = function (e, obj) {
+    p.onStateEvent = function(e, obj) {
         this.changeState(obj.state);
     }
-    p.disposeCurrentScene = function () {
+    p.disposeCurrentScene = function() {
         if (this.currentScene != null) {
             stage.removeChild(this.currentScene);
-            if(this.currentScene.dispose){
-               // this.currentScene.dispose();
+            if (this.currentScene.dispose) {
+                // this.currentScene.dispose();
             }
             this.currentScene = null;
         }
     }
-    p.gameStateMainMenu = function (tickEvent) {
+    p.gameStateMainMenu = function(tickEvent) {
         var scene = new game.GameMenu();
-        scene.on(game.GameStateEvents.GAME, this.onStateEvent, this, true, {state:game.GameStates.GAME});
+        scene.on(game.GameStateEvents.GAME, this.onStateEvent, this, true, { state: game.GameStates.GAME });
         stage.addChild(scene);
         this.disposeCurrentScene();
         this.currentScene = scene;
         this.changeState(game.GameStates.RUN_SCENE);
     }
-    p.gameStateGame = function (tickEvent) {
+    p.gameStateGame = function(tickEvent) {
         var scene = new game.Game();
-        scene.on(game.GameStateEvents.GAME_OVER, this.onStateEvent, this, true, {state:game.GameStates.GAME_OVER});
+        scene.on(game.GameStateEvents.GAME_OVER, this.onStateEvent, this, true, { state: game.GameStates.GAME_OVER });
         stage.addChild(scene);
         this.disposeCurrentScene()
         this.currentScene = scene;
         this.changeState(game.GameStates.RUN_SCENE);
     }
-    p.gameStateGameOver = function (tickEvent) {
+    p.gameStateGameOver = function(tickEvent) {
         var scene = new game.GameOver();
         stage.addChild(scene);
-        scene.on(game.GameStateEvents.MAIN_MENU, this.onStateEvent, this, true, {state:game.GameStates.MAIN_MENU});
-        scene.on(game.GameStateEvents.GAME, this.onStateEvent, this, true, {state:game.GameStates.GAME});
+        scene.on(game.GameStateEvents.MAIN_MENU, this.onStateEvent, this, true, { state: game.GameStates.MAIN_MENU });
+        scene.on(game.GameStateEvents.GAME, this.onStateEvent, this, true, { state: game.GameStates.GAME });
         this.disposeCurrentScene();
         this.currentScene = scene;
         this.changeState(game.GameStates.RUN_SCENE);
     }
-    p.gameStateRunScene = function (tickEvent) {
+    p.gameStateRunScene = function(tickEvent) {
         if (this.currentScene.run) {
             this.currentScene.run(tickEvent);
         }
     }
-    p.onTick = function (e) {
+    p.onTick = function(e) {
         if (this.currentGameStateFunction != null) {
             this.currentGameStateFunction(e);
         }
